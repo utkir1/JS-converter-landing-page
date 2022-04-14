c83f2e65ff3e2edb543959b435cc9f84abd1be52
utkir1 <otkirsoatboyev1@gmail.com>
2022-04-14 09:05:26 +0500

    converter is finished

---
+++ js/script.js
@@ -0,0 +1,64 @@
+var elCur1 = document.querySelector("#input1");
+var elCur2 = document.querySelector("#input2");
+var elLabel1 = document.querySelector("#val1");
+var elLabel2 = document.querySelector("#val2");
+var elConvertBtn = document.querySelector(".form-convert");
+var elExchangeBtn = document.querySelector("#changebtn");
+var elClearBtn = document.querySelector("#clear-btn");
+var elList = document.querySelector("ol");
+elLabel1.textContent = "$USD";
+elLabel2.textContent = "UZS";
+usdValue = 11348;
+
+elExchangeBtn.addEventListener("click", (e) => {
+  elCur1.value = "";
+  elCur2.value = "";
+  if (elLabel1.textContent == "$USD"){
+    elLabel1.textContent = "UZS";
+    elLabel2.textContent = "$USD";
+  }else {
+    elLabel1.textContent = "$USD";
+    elLabel2.textContent = "UZS";
+  }
+})
+
+elConvertBtn.addEventListener("click", function (evt){
+evt.preventDefault();
+
+var firstCurrency = elCur1.value.trim();
+var secondCurrency = elCur2.value.trim();
+if(elLabel1.textContent == "$USD") {
+  elCur2.value = Number(elCur1.value) * usdValue;
+}else {
+  elCur2.value = (Number(elCur1.value) / usdValue).toFixed(4);
+}
+if(isNaN(elCur2.value)) {
+  elCur2.value = "";
+  elCur1.value = "";
+  elCur1.style= "border-color:red";
+}else {
+  elCur1.style= "border-color:black";
+}
+
+var newLi = document.createElement("li");
+var newP = document.createElement("p");
+if(elLabel1.textContent == "$USD"){
+  newP.textContent = `${elCur1.value}$ => ${elCur2.value} so'm`;
+  newP.style.color = "blue"
+} else {
+  newP.textContent = `${elCur1.value} so'm => ${elCur2.value}$`;
+  newP.style.color = "green"
+}
+if(elLabel1.value == ""){
+  newP.textContent = ""
+}
+newLi.append(newP);
+elList.append(newLi);
+})
+
+
+
+
+clearBtn.addEventListener("click", (evt) => {
+  newLi.newP = ""
+})
